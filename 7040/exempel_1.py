a50531121d443dae3add05a718a428c683fcdfa9
Christoffer Palmquist <Christoffer.Palmquist06@skola.stenungsund.se>
2024-08-27 16:17:09 +0200

    ny

---
+++ 7040/exempel_1.py
@@ -0,0 +1,8 @@
+namn = input("Vad heter du?")
+print("Hej", namn, "!")
+
+favoritfärg = input("vad är din favoritfärg?")
+print( namn,"din favoritfärg är", favoritfärg)
+
+
+
---
+++ 7040/exempel_2.py
@@ -0,0 +1,10 @@
+namn = input("Vad heter du?")
+print ( namn, "är ett fint namn")
+
+
+mat = input("vad är din favorit mat")
+print (namn, mat, "är verkligen gott")
+
+input("hur mycket pengar har du")
+print (namn, "du är rik")
+
