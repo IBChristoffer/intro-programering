3339c0b1591c2bec3411cd0f4c9b14a3cd96ccb4
Christoffer Palmquist <Christoffer.Palmquist06@skola.stenungsund.se>
2024-09-03 15:55:45 +0200

    hej

---
+++ 7045/uppgift1.py
@@ -0,0 +1,7 @@
+höjd = float(input("vad är ektangelns höjd: "))
+bredd = float(input("vad är ektangelns bredd: "))
+area = bredd * höjd / 2
+omkrets = 2 * (bredd + höjd)
+print("arean är:", area)
+
+
---
+++ 7045/uppgift2.py
@@ -0,0 +1,5 @@
+radien = float(input("vad är cirkelns radie: "))
+area = radien * radien * 3.14
+omkrets = 2 * radien * 3.14
+print("arean är:",area )
+print("omkretsen är:",omkrets )
